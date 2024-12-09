fa49fc7fcc635cd5e0b06bece5966ee6de7d5e90
PerHac13 <abdullah13112004@gmail.com>
2024-12-09 16:41:07 +0530

    chore: configure files

---
+++ setup.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+# setup.sh
+# Example: Install additional dependencies or setup project-specific configs
+echo "Setting up workspace..."
+echo "Installing dependencies..."
+echo "Done!"
